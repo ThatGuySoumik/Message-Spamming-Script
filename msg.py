46f5d8be760ed36b3aa8515a04592d5d2b6d2af8
Soumik <log.soumik@gmail.com>
2026-08-06 12:20:50 +0530

    Message Spaning Script

---
+++ msg.py
@@ -0,0 +1,25 @@
+import pyautogui as pyg
+import time
+import pygame as pg
+
+pg.mixer.init()
+
+
+def type_msg(msg):
+    pyg.write(msg)
+    pyg.press('enter')
+
+time.sleep(4) #run after 8 seconds
+i=0
+while i<=24: 
+    msg = "Puii" #enter msg want to send
+    type_msg(msg)
+    i+=1
+
+pg.mixer.music.load("khatam.mp3")
+pg.mixer.music.play()
+
+while pg.mixer.music.get_busy():
+    time.sleep(0.1)
+    
+print("Task Complete")
